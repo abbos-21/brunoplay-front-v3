27b103b75e2cebfeab082f6a732c5eb04fc6c16e
abbos-21 <abbos.nazariy0621@gmail.com>
2026-03-21 13:37:14 +0500

    commit 8

--- src/components/EnergyLevel.vue
+++ src/components/EnergyLevel.vue
@@ -5,7 +5,7 @@ import { EnergyImage } from '@/assets/images'
 
 <template>
   <div class="flex items-center p-2 px-4 gap-2 relative bg-[#fbe166] rounded-[50px] shadow-card">
-    <h1 class="font-bold outlined [--outline:#C17E23]">1600</h1>
+    <h1 class="font-bold outlined [--outline:#C17E23]">1500</h1>
     <span class="h-4 w-px bg-[#C17E23]"></span>
     <div class="w-10"></div>
     <LevelSVG class="w-20 absolute inset-y-0 -right-5" />
